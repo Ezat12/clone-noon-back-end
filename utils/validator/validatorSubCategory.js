093106e6d6eca3c8f06d841e70f2ce89a0a94585
Ezat <ezatelbery164@gmail.com>
2024-08-21 17:56:32 +0300

    add change

--- utils/validator/validatorSubCategory.js
+++ utils/validator/validatorSubCategory.js
@@ -1,6 +1,7 @@
 const { check } = require("express-validator");
 const validatorError = require("../../middlewares/validatorError");
 const slugify = require("slugify");
+const SubCategory = require("../../models/subCategoryModel");
 
 const validatorCreateSubCategory = [
   check("name")
@@ -8,10 +9,13 @@ const validatorCreateSubCategory = [
     .withMessage("The name is required")
     .isLength({ min: 2, max: 30 })
     .withMessage(
-      "the SubCategory name should be min length 4 and max length 30"
+      "the SubCategory name should be min length 2 and max length 30"
     )
-    .custom((val, { req }) => {
+    .custom(async (val, { req }) => {
       req.body.slug = slugify(val);
+      const checkSubCategory = await SubCategory.findOne({ name: val });
+
+      if (checkSubCategory) throw new Error("The name is already exist");
       return true;
     }),
   check("category")
